b24bab69a55fc0390a6a21e085d00871c51f45bd
Mehrab Hafiz <mehrabhfz@gmail.com>
2021-02-23 02:09:43 -0500

    Additional clusters

--- kmeans.py
+++ kmeans.py
@@ -10,19 +10,23 @@ features = dataset[:, 2:]
 labels = dataset[:, 1]
 
 X_train, X_test, y_train, y_test = train_test_split(features, labels, 
-                                                    test_size=.10, shuffle=True)
+                                                    test_size=.30, shuffle=True)
 
-kmeans = KMeans(n_clusters=2).fit(X_train)
+kmeans = KMeans(n_clusters=4).fit(X_train)
 predictions = kmeans.predict(X_test)
 
 #for i in range(40):
     #print(predictions[i], y_test[i])
     
-clusters = {0: {'B': 0, 'M': 0}, 
-            1: {'B': 0, 'M': 0} }
+clusters = {0: {'B': 0, 'M': 0},
+            1: {'B': 0, 'M': 0},
+            2: {'B': 0, 'M': 0},
+            3: {'B': 0, 'M': 0}}
 
 for i in range(len(predictions)):
     clusters[predictions[i]][y_test[i]] += 1
     
 print('Cluster 0\n -', clusters[0])
 print('Cluster 1\n -', clusters[1])
+print('Cluster 2\n -', clusters[2])
+print('Cluster 3\n -', clusters[3])
